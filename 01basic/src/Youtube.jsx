0488cca24fd098567168f72395a3fd2449ae5b6f
Dipsundar Panja <panjadipsundar@gmail.com>
2025-02-16 02:47:32 +0530

    this is npx create project

---
+++ 01basic/src/Youtube.jsx
@@ -0,0 +1,7 @@
+function Youtube(){
+    return(
+        <h1>react project start on 16 Feb</h1>
+    )
+}
+
+export default Youtube
---
+++ 01viteproject/src/App.jsx
@@ -0,0 +1,13 @@
+import Youtube from "./Youtube";
+
+function App() {
+  return (
+    <>
+      <h1>Vite Project</h1>
+      <h2>Vite Project</h2>
+      <Youtube />
+    </>
+  );
+}
+
+export default App;
